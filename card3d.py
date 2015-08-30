a09a35d1caafaf0e388a6007bd8963daf50bcd49
Jonathan D. Lettvin <jlettvin@gmail.com>
2015-08-30 16:44:30 -0400

    Clarified, simplified, improved

--- card3d.py
+++ card3d.py
@@ -3,7 +3,7 @@
 from sys import (path)
 path.append('Dict')
 
-from visual import (label, frame, vector, rate)
+from visual import (scene, frame, rate, label, vector, exit)
 from select import (select)
 from socket import (socket, AF_INET, SOCK_STREAM)
 from ConfigParser import (ConfigParser)
@@ -11,10 +11,11 @@ from Dict import (Dict)
 
 class Card(label):
     cardno = 1
+    constant = Dict(g=9.8, dt=0.01)
 
     def __init__(self, dct):
         super(Card, self).__init__(**dct.__dict__)
-        (self.g, self.dt, self.text) = (dct.g, dct.dt, dct.text)
+        (self.force, self.text) = (vector(0.0,0.0,0.0), dct.text)
         (self.velocity, self.card) = (dct.velocity, dct.card)
         (self.msg) = (dct.text)
         Card.cardno += 1
@@ -28,7 +29,7 @@ class Card(label):
     def __call__(self, text=""):
         self.msg = text if text else self.msg
         if self.visible:
-            dxyz = self.velocity * self.dt
+            dxyz = self.velocity * Card.constant.dt
             (self.pos, self.dy) = (self.pos + dxyz, dxyz.y)
             if self.y <= self.radius:
                 if (abs(self.dy) > 1e-2):
@@ -39,16 +40,15 @@ class Card(label):
                     self.y = self.radius
                 self.report()
             else:
-                self.velocity.y = self.velocity.y - self.g * self.dt
+                self.velocity.y = (
+                        self.velocity.y - Card.constant.g * Card.constant.dt)
 
-if __name__ == "__main__":
-    configname = 'card3d.cfg'
+def getCards(f, configname = 'card3d.cfg'):
     config = ConfigParser()
     config.read(configname)
-    (cards, f) = ({}, frame())
-    f.axis = (0,1,0)
+    cards = {}
     dct = Dict(
-            g=9.8, dt=0.01, velocity=vector(0,-1,0),
+            velocity=vector(0,-1,0),
             color=(1,1,1), background=(0.25,0.25,0.25), opacity=0.2,
             keep=1.0, frame=f,
             visible=True)
@@ -63,32 +63,28 @@ if __name__ == "__main__":
         (dct.pos, dct.card, dct.text) = ((num % 4, 4, num % 4), dig, line)
         dct.radius = 1
         cards[key] = Card(dct)
+    return cards
 
-    (host, port, backlog, size) = ('', 50000, 5, 1024)
-    s = socket(AF_INET, SOCK_STREAM) 
-    s.bind((host,port)) 
-    s.listen(backlog) 
+def getmessage(s):
+    msg = ""
+    rcv, snd, exc = select([s,], [], [], 0)
+    if rcv != []:
+        client, address = s.accept() 
+        msg = data = client.recv(size) 
+        abc = list(msg.partition('/?'))
+        if abc[1]:
+            msg = abc[2]
+        abc = list(msg.partition(' '))
+        if abc[1]:
+            msg = abc[0]
+        if msg: 
+            client.send(msg)
+        client.close()
+    return msg
 
-    def getmessage():
-        msg = ""
-        rcv, snd, exc = select([s,], [], [], 0)
-        if rcv != []:
-            client, address = s.accept() 
-            msg = data = client.recv(size) 
-            abc = list(msg.partition('/?'))
-            if abc[1]:
-                msg = abc[2]
-            abc = list(msg.partition(' '))
-            if abc[1]:
-                msg = abc[0]
-            if msg: 
-                client.send(msg)
-            client.close()
-        return msg
-
-    while 1:
-        f.rotate(angle=0.01)
-        msg = getmessage()
+def main(cards, f, s):
+    while True:
+        msg = getmessage(s)
         cardno = None
         abc=list(msg.partition('_'))
         if abc[1]:
@@ -108,3 +104,71 @@ if __name__ == "__main__":
                 with open(configname, 'w') as target:
                     config.write(target)
             card(text)
+
+if __name__ == "__main__":
+
+    # Initialize the web interface.
+    (host, port, backlog, size) = ('', 50000, 5, 1024)
+    s = socket(AF_INET, SOCK_STREAM) 
+    s.bind((host,port)) 
+    s.listen(backlog) 
+
+    # Initialize the visual python scene.
+    scene.exit = True
+    scene.title = 'card3d: prototype'
+    f = frame()
+
+    # Initialize the card contents from the configuration file.
+    cards = getCards(f, 'card3d.cfg')
+
+    # Define the spinning functions
+    def spinLeft(f):
+        f.axis = (1, 0, 0)
+        f.rotate(angle=0.1)
+
+    def spinRight(f):
+        f.axis = (1, 0, 0)
+        f.rotate(angle=0.1)
+
+    def spinForward(f):
+        f.axis = (0, 1, 0)
+        f.rotate(angle=0.1)
+
+    def spinBackward(f):
+        f.axis = (0, 1, 0)
+        f.rotate(angle=0.1)
+
+    def spinUp(f):
+        f.axis = (0, 0, 1)
+        f.rotate(angle=0.1)
+
+    def spinDown(f):
+        f.axis = (0, 0, 1)
+        f.rotate(angle=0.1)
+
+    # Prepare a dictionary for keyboard function binding.
+    spin = Dict(
+        h=spinLeft, a=spinLeft,
+        l=spinRight, f=spinRight,
+        j=spinDown, s=spinDown,
+        d=spinUp, k=spinUp,
+        y=spinForward, t=spinForward,
+        b=spinBackward, v=spinBackward
+    )
+
+    # Define keyboard event function.
+    def keyInput(evt):
+        k = evt.key
+        if len(k) == 1:
+            if k in spin.__dict__.keys():
+                spin.__dict__[k](f)
+            elif k == 'q' or k == 'Q':
+                exit()
+        elif ((k == 'backspace') or (k == 'delete')):
+            pass
+
+    # Bind keyboard events to the function.
+    scene.bind('keydown', keyInput)
+
+    # Run the card3d main loop.
+    main(cards, f, s)
